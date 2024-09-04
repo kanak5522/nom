85d4479d3be5160293c375cca0babd9be6109dc9
kanak5522 <kanakchandel552000@gmail.com>
2024-09-04 02:32:49 -0700

    files

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "newinnn3t" {
+    ami = "ami-04e49d62cf88738f1"
+    instance_type = "t2.micro"
+  
+}
+
+
+resource "aws_instance" "n3t" {
+    ami = "ami-04e49d62cf88738f1"
+    instance_type = "t2.micro"
+  
+}
+
+resource "aws_instance" "net" {
+    ami = "ami-04e49d62cf88738f1"
+    instance_type = "t2.micro"
+  
+}
+
+resource "aws_instance" "net123" {
+    ami = "ami-04e49d62cf88738f1"
+    instance_type = "t2.micro"
+  
+}
+resource "aws_instance" "knkinstance" {
+    ami = "ami-04e49d62cf88738f1"
+    instance_type = "t2.micro"
+  
+}
+
+
+resource "aws_instance" "instance3" {
+    ami = "ami-04e49d62cf88738f1"
+    instance_type = "t2.micro"
+  
+}
+resource "aws_instance" "instanc23e3" {
+    ami = "ami-04e49d62cf88738f1"
+    instance_type = "t2.micro"
+  
+}
+resource "aws_instance" "example" {
+  ami           = "ami-04e49d62cf88738f1"
+  instance_type = "t2.micro"              
+ 
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
